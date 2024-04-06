38bc4a2630cd474d2cd086799678fce89ad513f0
Anphimens <philipinamensah19@gmail.com>
2024-04-06 00:04:33 -0700

    making the files executable

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["54.87.242.205", "100.26.212.66"]
 
 
 def do_deploy(archive_path):
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["54.87.242.205", "100.26.212.66"]
 
 
 def do_pack():
